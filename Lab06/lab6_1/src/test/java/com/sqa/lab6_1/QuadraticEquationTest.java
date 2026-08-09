76603b74a7645da078ecd7ba9c053459f9e96eee
kaokao31 <kanditat.n@kkumail.com>
2026-08-09 14:08:11 +0700

    Add lab6_1 for JunitTest and Update template_Lab6.xlsx

---
+++ Lab06/lab6_1/src/test/java/com/sqa/lab6_1/QuadraticEquationTest.java
@@ -0,0 +1,26 @@
+package com.sqa.lab6_1;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class QuadraticEquationTest {
+
+    private final QuadraticEquation quadraticEquation = new QuadraticEquation();
+
+    @ParameterizedTest(name = "Rule#{index} => a={0}, b={1}, c={2}, expected={3}")
+    @CsvSource({
+            // a,  b,  c,  expectedRootNature
+            "1,  5,  6,  REAL_ROOTS",
+            "1,  2,  1,  EQUAL_ROOTS",
+            "1,  1,  1,  IMAGINARY_ROOTS",
+            "0,  1,  1,  NOT_QUADRATIC"
+    })
+    void determineRootNature_limitedEntryDecisionTable(int a, int b, int c, RootNature expected) {
+        RootNature actual = quadraticEquation.determineRootNature(a, b, c);
+        assertEquals(expected, actual,
+                () -> "a=" + a + ", b=" + b + ", c=" + c
+                        + " expected " + expected + " but got " + actual);
+    }
+}
